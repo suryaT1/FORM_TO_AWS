25e77c99866d62d4c4b7fa29a1c15dd4b35336e2
Surya_T <71425657+suryaT1@users.noreply.github.com>
2025-04-17 14:25:46 +0530

    🔄 Auto-update tfvars via Flask form

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "vpc" {
+     default = "126252552"
+    }
+    variable "sg_grp" {
+     default = "allow-all"
+    }
+    variable "server_name" {
+        default = "abc"
+    }
+    variable "instance_type" {
+        default = "t2.micro"
+    }
+    variable "region" {
+        default = "us-east-1"
+    }
+    variable "ami_id" {
+        default = "abc"
+    }
+    variable "key_name" {
+        default = "abc"
+    }
